5040cfbd318067f83e21193e55b7a751805be87d
Vineeth S <svineethsaji@gmail.com>
2024-04-12 18:33:44 +0530

    NexTT OTT App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://www.squadhelp.com/story_images/visual_images/9023437.jpg"  alt="NexTTLogo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          NexTT is an OTT application for all Generations
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
